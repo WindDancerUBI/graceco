fb49bb09ab0c1f499fb113e8c357d4ed7f0def5b
huangjitao <164173925@qq.com>
2023-04-20 21:27:48 +0800

    chore: dumi部署

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,12 @@
 /*
- * @Title: your project
+ * @Title: dumi配置
  * @Author: huangjitao
  * @Date: 2023-04-20 19:08:14
  * @Description: description of this file
  */
 import { defineConfig } from 'dumi';
+
+const repo = 'graceco';
 
 export default defineConfig({
   outputPath: 'docs-dist',
@@ -20,12 +22,9 @@ export default defineConfig({
     // 配置入口文件路径，API 解析将从这里开始
     entryFile: './src/index.ts',
   },
-  base:
-    process.env.NODE_ENV === 'production'
-      ? `https://github.com/WindDancerUBI/graceco/`
-      : '/',
-  publicPath:
-    process.env.NODE_ENV === 'production'
-      ? `https://github.com/WindDancerUBI/graceco/`
-      : '/',
+  // 使用gh-pages部署
+  base: `/${repo}/`,
+  publicPath: process?.env?.NODE_ENV === 'development' ? `/${repo}/` : './',
+  // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
+  exportStatic: {},
 });
