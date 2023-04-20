10c41494852da4274626d8129f8b07527843b85a
huangjitao <164173925@qq.com>
2023-04-20 21:21:45 +0800

    chore: dumi配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,3 +1,9 @@
+/*
+ * @Title: your project
+ * @Author: huangjitao
+ * @Date: 2023-04-20 19:08:14
+ * @Description: description of this file
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
@@ -6,12 +12,20 @@ export default defineConfig({
     name: 'graceco',
   },
   locales: [
-    { id: "zh-CN", name: "中文" },
-    { id: "en-US", name: "English" },
+    { id: 'zh-CN', name: '中文' },
+    { id: 'en-US', name: 'English' },
   ],
   apiParser: {},
   resolve: {
     // 配置入口文件路径，API 解析将从这里开始
     entryFile: './src/index.ts',
   },
+  base:
+    process.env.NODE_ENV === 'production'
+      ? `https://github.com/WindDancerUBI/graceco/`
+      : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? `https://github.com/WindDancerUBI/graceco/`
+      : '/',
 });
